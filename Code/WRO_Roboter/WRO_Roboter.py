e4f81b8a25f7b12e5bc53a4e1e273855d32ac38a
Otis2510 <panagiotiskara8@gmail.com>
2025-03-03 14:15:58 +0100

    Grundfunktionen der Motoren und Sensoren. Deklaration + Kurzer Test

--- Code/WRO_Roboter/WRO_Roboter.py
+++ Code/WRO_Roboter/WRO_Roboter.py
@@ -1,1 +1,60 @@
-print("Grüß Gott")
+from re import M
+import time
+from Examples.test import Motor
+from gpiozero import DistanceSensor
+from gpiozero import LineSensor
+import RPi.GPIO as GPIO
+from board import SCL, SDA
+import busio 
+from adfruit_pca9685 import PCA9685
+from adfruit_motor import motor
+from time import sleep
+
+# Ultraschallsensor wird deklariert und kalibriert
+
+Tr = 23
+Ec = 24
+
+U_Sensor = DistanceSensor(echo=Ec, trigger=Tr,max_distance=2)
+
+# Daten auslesen
+
+def ReadData():
+    return (U_sensor.distance) *100 # *100 um cm zu erhalten
+
+# Motor wird deklariert und kalibriert
+
+Motor_PosIn = 15 
+Motor_NegIN = 14
+Motor_ENA = 7
+
+GPIO.setup(Motor_PosIn, GPIO.OUT)
+GPIO.setup(Motor_NegIN, GPIO.OUT)
+GPIO.setup(Motor_ENA, GPIO.OUT)
+
+# PWM auf ENA-Pin, um die Geschwindigkeit zu steuern
+pwm = GPIO.PWM(ENA, 100)
+pwm.start(100)
+
+# Motor-Grundfunktionen definieren
+
+def motor_vorwärts():
+    GPIO.output(Motor_PosIn, GPIO.HIGH)
+    GPIO.output(Motor_NegIN, GPIO.LOW)
+    print("Motor läuft vorwärts")
+
+def motor_rückwärts():
+    GPIO.output(Motor_PosIn, GPIO.LOW)
+    GPIO.output(Motor_NegIN, GPIO.HIGH)
+    print("Motor läuft rückwärts")
+
+def motor_stoppen():
+    GPIO.output(Motor_PosIn, GPIO.LOW)
+    GPIO.output(Motor_NegIN, GPIO.LOW)
+    print("Motor gestoppt")
+
+motor_vorwärts()
+time.sleep(6)
+motor_rückwärts()
+time.sleep(6)  
+motor_stoppen()
