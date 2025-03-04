b5320ecfd1bcb42ad2d1cbdb2983de556fd48769
NikiE2812 <eberennzniklas@gmail.com>
2025-03-04 10:15:28 +0100

    Datei für test angepasst

--- Code/WRO_Roboter/lenkung_funktion.py
+++ Code/WRO_Roboter/lenkung_funktion.py
@@ -2,7 +2,7 @@ import time
 from board import SCL, SDA
 import busio
 from adafruit_motor import servo
-from adafruit_pca9685 import PCA
+from adafruit_pca9685 import PCA9685
 
 i2c = busio.I2C(SCL,SDA)
 pca = PCA9685(i2c, address=0x5f)
@@ -11,7 +11,7 @@ pca.frequency = 50
 
 
 def set_angle(ID, angle):
-    servo_angle = servo.Servo(pca.channel[ID], min_pulse=500, max_pulse=2400, actuation_range=180)
+    servo_angle = servo.Servo(pca.channels[ID], min_pulse=500, max_pulse=2400, actuation_range=180)
     servo_angle.angle = angle
 
 def lenken_links():
--- Code/WRO_Roboter/test.py
+++ Code/WRO_Roboter/test.py
@@ -8,20 +8,20 @@ import lenkung_funktion
 import MotorAnsteuerung
 
 try:
+
   while True: 
     if Ultraschallsensor.checkdist() <= 30:
+      print('Distance ist ' + Ultraschallsensor.checkdist())
       MotorAnsteuerung.Motor_Fahren(0.35)
       lenkung_funktion.lenken_links() 
-    if Ultraschallsensor.checkdist() <= 50:
+    elif Ultraschallsensor.checkdist() <= 50:
+      print('Distance ist ' + Ultraschallsensor.checkdist())
       MotorAnsteuerung.Motor_Fahren(0.5)
       lenkung_funktion.lenken_gerade()
-    MotorAnsteuerung.Motor_Fahren(1)
-    lenkung_funktion.lenken_gerade()
-
-
-
-
-
+    else:
+      MotorAnsteuerung.Motor_Fahren(1)
+      lenkung_funktion.lenken_gerade()
+      print('Distance ist ' + Ultraschallsensor.checkdist())
 
 except KeyboardInterrupt:
   MotorAnsteuerung.Motor_Fahren(0)
