1cc725e57db14ba139b7d6f2846d6aa95a04e4da
NikiE2812 <eberenzniklas@gmail.com>
2025-03-04 08:18:17 +0100

    Code leicht abgeändert bei MotorAnsteuerung.py

--- Code/WRO_Roboter/MotorAnsteuerung.py
+++ Code/WRO_Roboter/MotorAnsteuerung.py
@@ -1,27 +1,29 @@
+
 import time
 from adafruit_pca9685 import PCA9685
-from Adafruit_MotorHAT import DC_Motor
-
+from adafruit_motor import motor
+import busio
+from board import SCL, SDA
+import time
 #Bestimmung der Pole die an den Motor angeschlossen sind
 DCMotor_IN1 = 15 #positiver Pol
 DCMotor_IN2 = 14 #negativer Pol
 
 
 #Einrichtung der I2C Adresse
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(SCL, SDA)
 
 #Erstellung/Deklarierung des Motors und Geschwindigkeit
 
-pwm = PCA9685(i2c, address=0x5f)
-pwm.frequency = 1000
+pwm_motor = PCA9685(i2c, address=0x5f)
+pwm_motor.frequency = 1000
 
-DC_Motor = motor.DCMotor(pwm.channels[DCMotor_IN1], pwm.motor.channels[DCMotor_IN2])
+DC_Motor = motor.DCMotor(pwm_motor.channels[DCMotor_IN1], pwm_motor.channels[DCMotor_IN2])
 
 
 DC_Motor.duty_cycle = 4000
 
 time.sleep(2)
-
 DC_Motor.duty_cycle = 1000
 
 time.sleep(2)
