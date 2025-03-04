c7d1b61c194dc5d5018377d724d52ad620a0cd83
Otis2510 <panagiotiskara8@gmail.com>
2025-03-04 08:33:21 +0100

    Statt duty_cycle wird jetzt throttle verwendet

--- Code/WRO_Roboter/MotorAnsteuerung.py
+++ Code/WRO_Roboter/MotorAnsteuerung.py
@@ -4,7 +4,7 @@ from adafruit_pca9685 import PCA9685
 from adafruit_motor import motor
 import busio
 from board import SCL, SDA
-import time
+
 #Bestimmung der Pole die an den Motor angeschlossen sind
 DCMotor_IN1 = 15 #positiver Pol
 DCMotor_IN2 = 14 #negativer Pol
@@ -19,13 +19,14 @@ pwm_motor = PCA9685(i2c, address=0x5f)
 pwm_motor.frequency = 1000
 
 DC_Motor = motor.DCMotor(pwm_motor.channels[DCMotor_IN1], pwm_motor.channels[DCMotor_IN2])
+DC_Motor.decay_mode = (motor.SLOW_DECAY)
 
+#Fahren
 
-DC_Motor.duty_cycle = 4000
+DC_Motor.throttle = 4000
+time.sleep(5)
 
-time.sleep(2)
-DC_Motor.duty_cycle = 1000
+DC_Motor.throttle = 1000
+time.sleep(5)
 
-time.sleep(2)
-
-DC_Motor.duty_cycle = 0
+DC_Motor.throttle = 0
