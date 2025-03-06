f77d8c3764e72cb3c0ab74914cf270dd529a0c13
Otis2510 <panagiotiskara8@gmail.com>
2025-03-06 11:35:13 +0100

    CameraColorDetection hinzugefügt und Funktion dass der Roboter nach 3 Runden stoppt

---
+++ Code/WRO_Roboter/CameraColorDetection.py
@@ -0,0 +1,45 @@
+import time
+from turtle import width
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+def ColorDetection():
+
+	while True:
+		_, frame = cap.read()
+		hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		height, width, _ = frame.shape
+
+		cx = int(width / 2)
+		cy = int(height / 2)
+
+		pixel_center = hsv_frame[cy, cx]
+		hue_value = pixel_center[0]
+
+		color = "Undefined"
+
+		if 15 < hue_value < 35:
+			color = "ORANGE"
+			return color
+		elif 210 < hue_value < 235:
+			color = "BLUE"
+			return color
+		else:
+			color = "WHITE"
+			return color
+
+		print(pixel_center)
+		cv2.putText(frame, color, (10, 50), 0, 1, (255, 0, 0), 2)
+		cv2.circle(frame, (cx, cy), 5, (255, 0, 0), 3)
+	 
+		cv2.imshow("Frame", frame)
+		time.sleep()
+		#key = cv2.waitKey(1)
+		#if key == 27:
+		#break
+
+#cap.release()
+#cv2.destroyAllWindows()
--- Code/WRO_Roboter/test.py
+++ Code/WRO_Roboter/test.py
@@ -1,3 +1,4 @@
+from encodings.punycode import T
 import math
 import time
 from board import SCL, SDA
@@ -7,10 +8,35 @@ from adafruit_motor import motor
 import Ultraschallsensor
 import MotorAnsteuerung
 import test2
+import CameraColorDetection
+
+
+OrangeLine = False
+BlueLine = False
+CrossedOrangeLines = 0
+CrossedSection = 0
+RoundCounter = 0
 
 if __name__ == "__main__":
   try:
     while True:
+      distance = Ultraschallsensor.checkdist()
+      winkel = math.degrees(math.atan((distance+5)/distance))
+      DetectedColor = CameraColorDetection.ColorDetection()
+
+      if DetectedColor == "ORANGE":
+          OrangeLine = True
+          BlueLine = False
+
+      elif DetectedColor == "BLUE":
+          BlueLine = True
+          OrangeLine = False
+
+      if distance <= 90:
+        print(distance)
+        print(winkel)
+        test2.set_angle(0,winkel)
+
       distance = Ultraschallsensor.checkdist()   
       winkel = 90 + ((200 - distance) / (200 - 5)) * 90 
       winkel_gerundet = round(winkel) - 50 
@@ -21,8 +47,17 @@ if __name__ == "__main__":
         print('fertig')
 
       else:
-        test2.set_angle(1,90)
-        MotorAnsteuerung.Motor_Fahren(0.8)
+        test2.set_angle(0,35)
+        MotorAnsteuerung.Motor_Fahren(0.7)
+
+      if OrangeLine == True and BlueLine == True:
+          CrossedSection = CrossedSection + 1
+
+      if CrossedSection == 4:
+          break
+     
+      test2.set_angle(1,90)
+      MotorAnsteuerung.Motor_Fahren(0.8)
 
   except KeyboardInterrupt:
     MotorAnsteuerung.Motor_Fahren(0)
