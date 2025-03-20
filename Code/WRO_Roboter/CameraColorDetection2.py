428fe09fc41dafbcbbe3f0173306dad0912dc7a7
Otis2510 <panagiotiskara8@gmail.com>
2025-03-20 14:43:14 +0100

    gitignore datei und branch

--- Code/WRO_Roboter/CameraColorDetection2.py
+++ Code/WRO_Roboter/CameraColorDetection2.py
@@ -17,8 +17,9 @@ while True:
 
 	hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	print (hsv_frame.shape)
-
+	HueValueIsOrange = 0
+	HueValueIsBlue = 0
+	color = "Undefined"
 	height, width, _ = frame.shape
 
 	#cx = int(width / 2)
@@ -26,29 +27,34 @@ while True:
 
 	AreaStartPixelX = int(width/2) -5
 	AreaStartPixelY = int(height/2) - 5
+	AreaEndPixelX = int(width/2) +5
+	AreaEndPixelY = int(height/2) +5
 
-	roi = hsv_frame[AreaStartPixelY, AreaStartPixelX]
+	roi = hsv_frame[AreaStartPixelY:AreaEndPixelY, AreaStartPixelX:AreaEndPixelX]
 
 	#pixel_Area = hsv_frame[roi]
 	
 	hue_value = roi[:, :, 0]
 
-	print(hue_value)
+	for hue_value in roi:
+		if 9 < hue_value < 13:
+			HueValueIsOrange = HueValueIsOrange + 1
+		elif 105 < hue_value < 115:
+			HueValueIsBlue = HueValueIsBlue + 1
 
-
-	'''
-	color = "Undefined"
-
-	if 9 < hue_value < 13:
+	if HueValueIsOrange >= 20:
 		color = "ORANGE"
-		#return color
-	elif 95 < hue_value < 120:
+		print(color)
+	elif HueValueIsBlue >= 20:
 		color = "BLUE"
-		#return color
+		print(color)
 	else:
 		color = "WHITE"
-		#return color
+		print(color)
 
+	#highlightedArea = frame.copy()
+
+	#highlightedArea[hue_mask] = [0, 0, 255]
 #print(pixel_center)
 #cv2.putText(frame, color, (10, 50), 0, 1, (255, 0, 0), 2)
 #cv2.circle(frame, (cx, cy), 5, (255, 0, 0), 3)
@@ -61,5 +67,3 @@ while True:
 
 #cap.release()
 #cv2.destroyAllWindows()
-
-'''
