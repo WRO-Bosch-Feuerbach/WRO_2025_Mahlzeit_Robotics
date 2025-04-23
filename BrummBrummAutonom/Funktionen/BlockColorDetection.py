1b704019ecf261bfe4b0573b7518a8c60d7dc525
Otis2510 <panagiotiskara8@gmail.com>
2025-04-23 08:40:14 +0200

    Alle neuen relevnten Dateien (auch umbeannt)

---
+++ BrummBrummAutonom/Funktionen/BlockColorDetection.py
@@ -0,0 +1,43 @@
+from picamera2 import Picamera2
+import numpy as np
+import cv2
+import time
+
+# Kamera einstellen
+picam = Picamera2()
+picam.start()
+time.sleep(1)
+
+# HSV bereiche einstellen
+red_upper = np.array([140, 255, 255])
+red_lower = np.array([125, 100, 100])
+
+green_upper = np.array([60, 255, 255])
+green_lower = np.array([30, 100, 100])
+
+pixel_threshold = 150
+
+def Blockfarbe():
+  frame = picam.capture_array()
+  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  # Maske erstellen
+  red_mask = cv2.inRange(hsv, red_lower, red_upper)
+  green_mask = cv2.inRange(hsv, green_lower, green_upper)
+  # Rote und gruene Pixel zaehlen
+  red_count = cv2.countNonZero(red_mask)
+  green_count = cv2.countNonZero(green_mask)
+
+  # Zum ueberprüfen von HSV Werten
+  '''
+  hsv_pixel = hsv[0,0]
+  print('HSV vom Pixel:', hsv_pixel)
+  '''
+
+  # Schauen ob was rotes oder gruenes im Bild ist
+  if red_count > pixel_threshold:
+    Farbe = 'ROT'
+  elif green_count > pixel_threshold:
+    Farbe = 'GRUEN'
+  else:
+    Farbe = 'TUNGTUNGTUNGSAHUR'
+  return Farbe
---
+++ BrummBrummAutonom/Funktionen/Kursanpassung.py
@@ -0,0 +1,55 @@
+import ServoLenkung
+import MotorAnsteuerung
+import Ultraschallsensor
+import time
+
+#----------------------------- Variablen -----------------------------#
+
+VelocityBackwards = -0.5
+distanceHinten = Ultraschallsensor.checkdistHinten()
+distanceLinks = Ultraschallsensor.checkdistLinks()
+distanceRechts = Ultraschallsensor.checkdistRechts()
+
+#---------------------------------------------------------------------#
+
+
+
+def Kursanp_LinksFahren():
+
+    #---------- Kurs anpassen ----------#
+    while distanceHinten > 15:
+        MotorAnsteuerung.Motor_Fahren(0)
+        distanceHinten = Ultraschallsensor.checkdistHinten()
+        if 15 < distanceHinten < 40 and distanceRechts > 5:
+            ServoLenkung.set_angle(1, 90)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+        if 40 < distanceHinten < 75 and distanceRechts > 5:
+            ServoLenkung.set_angle(1, 60)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+        if distanceHinten > 75 and distanceRechts > 5:
+            ServoLenkung.set_angle(1, 30)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+        #--------- Rückwaerts wenn links nicht genug Platz ist ---------#
+        while distanceHinten > 75 and distanceRechts < 5:
+            ServoLenkung.set_angle(1, 90)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+
+def Kursanp_RechtsFahren():
+
+    #---------- Kurs anpassen ----------#
+    while distanceHinten > 15:
+        MotorAnsteuerung.Motor_Fahren(0)
+        distanceHinten = Ultraschallsensor.checkdistHinten()
+        if 15 < distanceHinten < 40 and distanceRechts > 5:
+            ServoLenkung.set_angle(1, 90)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+        if 40 < distanceHinten < 75 and distanceRechts > 5:
+            ServoLenkung.set_angle(1, 120)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+        if distanceHinten > 75 and distanceRechts > 5:
+            ServoLenkung.set_angle(1, 150)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
+        #--------- Rückwaerts wenn links nicht genug Platz ist ---------#
+        while distanceHinten > 75 and distanceRechts < 5:
+            ServoLenkung.set_angle(1, 90)
+            MotorAnsteuerung.Motor_Fahren(VelocityBackwards)
