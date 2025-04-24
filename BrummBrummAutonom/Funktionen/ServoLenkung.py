0ff7b34a4a73f4bf0e13b5dfb3742f9afc98bfdb
Otis2510 <panagiotiskara8@gmail.com>
2025-04-24 07:28:34 +0200

    Andere Methode zur ServoLenkung hinzugeüfgt

--- BrummBrummAutonom/Funktionen/ServoLenkung.py
+++ BrummBrummAutonom/Funktionen/ServoLenkung.py
@@ -2,8 +2,10 @@ import busio
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 from board import SCL, SDA
+import RPi.GPIO as GPIO
+import time
 
-
+#--------------------------------- erste Methode zur Ansteuerung ---------------------------------
 # Initialisiere I2C
 i2c = busio.I2C(SCL, SDA)
 
@@ -20,8 +22,23 @@ def set_angle(ID, angle):
     servo_angle = servo.Servo(pca.channels[ID], min_pulse=1000, max_pulse=2000, actuation_range=180)
     servo_angle.angle = angle  # Winkel wird gesetzt
 
-# Hier wird der Servo ganz nach rechts gelenkt (180 Grad)
-#set_angle(0, 180)  # Kanal 0, 180 Grad für ganz rechts
+#--------------------------------- zweite Methode zur Ansteuerung ---------------------------------#
+
+# GPIO-Pin definieren
+servo_pin = 17
+
+# GPIO-Modus einstellen
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+pwm = GPIO.PWM(servo_pin, 50) # PWM mit einer Frequenz von 50 Hz
+pwm.start(0)  # Initialisiere PWM mit 0% Duty Cycle (Servo wird nicht bewegt)
+
+def set_servo_angle(angle):
+    # Berechne die Pulsbreite für den gewünschten Winkel
+    pulse_width = (angle / 18) + 2  # Dies entspricht der Pulsbreite in ms
+    duty_cycle = (pulse_width / 20) * 100  # Berechne den Duty Cycle in %
+    pwm.ChangeDutyCycle(duty_cycle)  # Setze den Duty Cycle für den Servo
 
 if __name__ == "__main__":
   while True:
